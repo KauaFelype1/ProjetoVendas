8d79fbc52a9c73d4824f44cb19742c31f0df96bc
aluno <aluno@LAB-03-19.sistemafieto.rede>
2026-04-10 16:03:43 -0300

    cadastro do usuário completo

--- sistema/src/application/controller/CadastroClientesController.java
+++ sistema/src/application/controller/CadastroClientesController.java
@@ -1,13 +1,45 @@
 package application.controller;
 
+import java.util.List;
+
+import application.model.ClienteModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CadastroClientesController {
 
-	@FXML
+    @FXML
     private Button btnSalvar;
+
+    @FXML
+    private TableColumn<ClienteModel, String> colCpf;
+
+    @FXML
+    private TableColumn<ClienteModel, String> colEmail;
+
+    @FXML
+    private TableColumn<ClienteModel, Integer> colIdCliente;
+
+    @FXML
+    private TableColumn<ClienteModel, String> colNome;
+
+    @FXML
+    private TableColumn<ClienteModel, String> colSenha;
+
+    @FXML
+    private TableColumn<ClienteModel, String> colStatus;
+
+    @FXML
+    private TableView<ClienteModel> tabCliente;
+    
+    private ObservableList<ClienteModel> listaCliente;
 
     @FXML
     private TextField txtCPF;
@@ -16,12 +48,82 @@ public class CadastroClientesController {
     private TextField txtEmail;
 
     @FXML
-    private TextField txtNascimento;
-
-    @FXML
     private TextField txtNome;
 
     @FXML
-    private TextField txtTelefone;
+    private TextField txtSenha;
+    
+    ClienteModel cliente = new ClienteModel(0, null, null, null, null, null);
+    
+    public void salvar() {
+    	
+    	if(txtNome.getText().isEmpty() || txtCPF.getText().isEmpty() || txtEmail.getText().isEmpty() || txtSenha.getText().isEmpty()) {
+    		
+    		Alert mensage = new Alert(Alert.AlertType.ERROR);
+    		mensage.setTitle("Erro");
+    		mensage.setHeaderText(null);
+    		mensage.setContentText("Preencha todos os campos!");
+    		mensage.showAndWait();
+    		
+    	}else {
+    		
+    		
+    		cliente.setNome(txtNome.getText());
+    		cliente.setCpf(txtCPF.getText());
+    		cliente.setEmail(txtEmail.getText());
+    		cliente.setSenha(txtSenha.getText());
+    		cliente.setStatus("Ativo");
+
+    		if(cliente.Salvar()) {
+    		Alert mensage = new Alert(Alert.AlertType.CONFIRMATION);
+    		mensage.setTitle("Sucesso");
+    		mensage.setHeaderText(null);
+    		mensage.setContentText("Cadastro realizado com sucesso");
+    		mensage.showAndWait();
+    		
+    		LimparCampos();
+    		}
+    	
+    		
+    	}
+    	
+    	ListarClientes(null);
+    	
+    }
+    
+    @FXML
+    public void initialize() {
+    	
+    	txtNome.setOnAction(e->{txtCPF.requestFocus();});
+    	txtCPF.setOnAction(e->{txtEmail.requestFocus();});
+    	txtEmail.setOnAction(e->{txtSenha.requestFocus();});
+    	txtSenha.setOnAction(e->{salvar();});
+    	
+    	colIdCliente.setCellValueFactory(new PropertyValueFactory<>("id"));
+    	colNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+    	colCpf.setCellValueFactory(new PropertyValueFactory<>("cpf"));
+    	colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+    	colSenha.setCellValueFactory(new PropertyValueFactory<>("senha"));
+    	colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+    	
+    	ListarClientes(null);
+    	
+    }
+    
+    public void ListarClientes(String valor) {
+    	
+    	List <ClienteModel> clientes = cliente.ListarClientes(valor);
+    	listaCliente = FXCollections.observableArrayList(clientes);
+    	tabCliente.setItems(listaCliente);
+    	
+    }
+    
+    public void LimparCampos() {
+    	txtNome.clear();
+    	txtCPF.clear();
+    	txtEmail.clear();
+    	txtSenha.clear();
+    }
+    
 
 }
--- sistema/src/application/model/ClienteModel.java
+++ sistema/src/application/model/ClienteModel.java
@@ -3,8 +3,11 @@ package application.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import application.conexao;
+import javafx.scene.control.Alert;
 
 public class ClienteModel {
 	
@@ -13,13 +16,15 @@ public class ClienteModel {
 	private String cpf;
 	private String email;
 	private String senha;
+	private String statusCli;
 	
-	public ClienteModel(int idCli, String nome, String cpf, String email, String senha) {
+	public ClienteModel(int idCli, String nome, String cpf, String email, String senha, String statusCli) {
 		this.idCli=idCli;
 		this.nome=nome;
 		this.cpf=cpf;
 		this.email=email;
 		this.senha=senha;
+		this.statusCli=statusCli;
 	}
 	
 	public int getId() {return idCli;}
@@ -27,26 +32,47 @@ public class ClienteModel {
 	public String getCpf() {return cpf;}
 	public String getEmail() {return email;}
 	public String getSenha() {return senha;}
+	public String getStatus() {return statusCli;}
 	
 	public void setId(int idCli) {this.idCli=idCli;}
 	public void setNome(String nome) {this.nome=nome;}
 	public void setCpf(String cpf) {this.cpf=cpf;}
 	public void setEmail(String email) {this.email=email;}
 	public void setSenha(String senha) {this.senha=senha;}
+	public void setStatus(String statusCli) {this.statusCli=statusCli;}
 	
-	public void Salvar() {
+	public boolean Salvar() {
 		
 		try(Connection conn = conexao.getConnection();
-				PreparedStatement consulta = conn.prepareStatement("insert into cliente (nome, cpf, email, senha) value (?,?,?,?)");) {
+				PreparedStatement consulta = conn.prepareStatement("insert into cliente (nome, cpf, email, senha, statusCli) value (?,?,?,?,?)");) {
 			
 			consulta.setString(1, nome);
 			consulta.setString(2, cpf);
 			consulta.setString(3, email);
 			consulta.setString(4, senha);
+			consulta.setString(5, statusCli);
 			consulta.executeUpdate();
+			return true;
 			
 		}catch(Exception e) {
-			e.printStackTrace();
+
+			String msg = e.getMessage();
+			Alert alerta = new Alert(Alert.AlertType.ERROR);
+			alerta.setTitle("Erro");
+			alerta.setHeaderText(null);
+			
+			if(msg.contains("CPF inválido")) {
+	            alerta.setContentText("CPF inválido! Digite apenas os 11 números.");
+	        } else if(msg.contains("Email inválido")) {
+	            alerta.setContentText("Email inválido! Ex: exemplo@email.com");
+	        } else if(msg.contains("Duplicate entry")) {
+	            alerta.setContentText("CPF ou email já cadastrado!");
+	        } else {
+	            alerta.setContentText("Erro ao cadastrar: " + msg);
+	        }
+			alerta.show();
+			return false;
+			
 		}
 		
 	}
@@ -66,6 +92,46 @@ public class ClienteModel {
 			e.printStackTrace();
 			return false;
 		}
+		
+	}
+	
+	public List<ClienteModel> ListarClientes(String valor){
+		
+		List <ClienteModel> clientes = new ArrayList<ClienteModel>();
+		
+		try(Connection conn = conexao.getConnection();
+				PreparedStatement consulta = conn.prepareStatement("select * from cliente");
+				PreparedStatement consultaWhere = conn.prepareStatement("select * from cliente where nome like ? or cpf like ? or email like ?");) {
+			
+			ResultSet resultado=null;
+			
+			if(valor == null) {
+				resultado = consulta.executeQuery();
+			}else {
+				consultaWhere.setString(1, "%"+valor+"%");
+				consultaWhere.setString(2, "%"+valor+"%");
+				consultaWhere.setString(3, "%"+valor+"%");
+				resultado = consultaWhere.executeQuery();
+			}
+			
+			while(resultado.next()) {
+				ClienteModel c = new ClienteModel(
+						resultado.getInt("idCli"),
+						resultado.getString("nome"),
+						resultado.getString("cpf"),
+						resultado.getString("email"),
+						resultado.getString("senha"),
+						resultado.getString("statusCli")
+						);
+				clientes.add(c);
+				
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return clientes;
 		
 	}
 	
